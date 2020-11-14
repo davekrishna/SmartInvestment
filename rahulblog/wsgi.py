74ac1d21634046575e8618b390032ed99c1e4b3a
Krishna Dave <davekrishna98@gmail.com>
2020-11-14 23:32:58 +0530

    wsgi changed

--- rahulblog/wsgi.py
+++ rahulblog/wsgi.py
@@ -10,6 +10,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learnage.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rahulblog.settings')
 
 application = get_wsgi_application()
