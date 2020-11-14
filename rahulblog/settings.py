f443834f553d57bd958ee344d2334902510f0e87
Krishna Dave <davekrishna98@gmail.com>
2020-11-15 00:20:16 +0530

    database changes

--- rahulblog/settings.py
+++ rahulblog/settings.py
@@ -77,14 +77,17 @@ WSGI_APPLICATION = 'rahulblog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+# import dj_database_url
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=config('DATABASE_URL')
+#     )
+# }
 import dj_database_url
-
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
-
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
